134730e9a3af5b1addde35b61802109043a16f83
Zachary Davy <zacharyjdavy@gmail.com>
2014-03-05 14:16:07 -0600

    add tic tac toe interface
    added an interface for handling the game interaction while not impacting
    the flow of the controller requests

---
+++ spec/ring_ttt/interface_spec.clj
@@ -0,0 +1,55 @@
+(ns ring-ttt.interface-spec
+  (:require [ring-ttt.spec-helper :refer :all]
+            [speclj.core :refer :all]
+            [ring-ttt.interface :refer :all]))
+
+(describe "Interface Spec:"
+
+  (describe "#determine-winner"
+    (with-redefs [ttt.rules/draw? (fn [_] true)]
+      (it "returns 'draw' if the game is a draw"
+        (should= ring-ttt.constants/DRAW
+                 (determine-winner "data"))))
+
+    (it "returns 'player1' if they won"
+      (with-redefs [ttt.rules/draw?  (fn [_] false)
+                    ttt.rules/win? (fn [_ _] true)]
+        (should= ring-ttt.constants/PLAYER1
+                 (determine-winner "data"))))
+
+    (it "returns 'draw' if the game is a draw"
+        (should= ring-ttt.constants/PLAYER2
+      (with-redefs [ttt.rules/draw?  (fn [_] false)
+                    ttt.rules/win? (fn [_ _] false)]
+                 (determine-winner "data")))))
+
+  (describe "#return-data"
+    (it "returns the provided  data in a map"
+      (let [_fake-return-data {:board "board" :turn "turn"}]
+      (should= _fake-return-data
+               (return-data "board" "turn")))))
+
+  (describe "#player-type"
+    (it "returns propper players types for ':pvp'"
+      (should= :player (:player1 (player-type :pvp)))
+      (should= :player (:player2 (player-type :pvp))))
+
+    (it "returns propper players types for ':pvc'"
+      (should= :player   (:player1 (player-type :pvc)))
+      (should= :computer (:player2 (player-type :pvc)))))
+
+  (describe "#game-evaluation"
+    (it "returns game over if game is over?"
+      (with-redefs [ttt.rules/game-over? (fn [_] true)]
+      (should-invoke game-over-return {:with ["game-data"]}
+                     (game-evaluation "game-data"))))
+
+    (it "returns game data if game not over"
+      (with-redefs [ttt.rules/game-over? (fn [_] false)]
+      (should-invoke game-active-return {:with ["data"]}
+                     (game-evaluation "data")))))
+
+  (describe "#start-game"
+    (it "calls the computer move if computer player is first player"
+      (should-invoke play-game {:with [_fake-parsed-params]}
+                     (start-game _fake-parsed-params)))))
--- src/ring_ttt/interface.clj
+++ src/ring_ttt/interface.clj
@@ -1,7 +1,63 @@
-(ns ring-ttt.interface)
+(ns ring-ttt.interface
+  (:require [ring-ttt.constants :refer :all]
+            [ttt.rules :refer :all]
+            [ttt.board :refer :all]
+            [ttt.ai :refer :all]))
 
-(defn other-turn [params])
+(declare play-game move-game take-turn)
+
+(defn determine-winner [game-data]
+  (let [board (:board game-data)]
+    (if (draw? board) DRAW
+    (if (win? board :player1) PLAYER1 PLAYER2))))
+
+(defn return-data [board turn]
+  {:board board :turn turn})
+
+(defn game-over-return [game-data]
+  (assoc game-data :game-over true
+   :winner (determine-winner game-data)))
+
+(defn game-active-return [game-data]
+  (assoc game-data :game-over false))
+
+(def game-modes
+  { :pvc {:player1 :player
+          :player2 :computer}
+    :pvp {:player1 :player
+          :player2 :player}})
+
+(defn player-type [mode]
+  (mode game-modes))
+
+(defn game-evaluation [data]
+  (if (game-over? (:board data))
+    (game-over-return data)
+    (game-active-return data)))
+
+(defn start-game [game-parameters]
+  (let [board (:board game-parameters)
+        turn  (:turn  game-parameters)
+        mode  (:mode  game-parameters)]
+    (if (= :computer (turn (player-type mode)))
+          (play-game game-parameters)
+          (return-data board turn))))
 
 (defn play-game [game-parameters]
-  {:board (:board game-parameters)
-   :turn  (other-turn game-parameters)})
+  (game-evaluation (move-game game-parameters)))
+
+(defn take-turn [turn board index mode]
+  (if (= :computer (turn (player-type mode)))
+        (move board (best-move board turn) turn)
+        (move board (read-string    index) turn)))
+
+(defn move-game [game-parameters]
+  (let [board     (:board game-parameters)
+        mode      (:mode  game-parameters)
+        turn      (:turn  game-parameters)
+        index     (:move  game-parameters)
+        opponent  (change-turn turn)
+        next-board (take-turn turn board index mode)]
+        (if (= :computer (opponent (player-type mode)))
+          (return-data (take-turn opponent next-board "_" mode) turn)
+          (return-data next-board opponent))))
